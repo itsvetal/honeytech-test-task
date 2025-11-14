a8b0566312cca22785b14fe75dff4682550cbcb7
iTsVetal <vitaliikryskiv.development@gmail.com>
2025-11-14 12:00:53 +0200

    Conected routes from modules

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
+$modulesPath = base_path('app/Modules');
+$modules = array_filter(glob($modulesPath . '/*'), 'is_dir');
+foreach ($modules as $modulePath) {
+    $moduleName = basename($modulePath);
+    $routesFile = $modulePath . '/Routes/api.php';
+    if (file_exists($routesFile)) {
+        require $routesFile;
+    }
+}
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,15 @@ use App\Modules\Post\Http\Controllers\Web\PostController;
 use App\Modules\Tag\Http\Controllers\Web\TagController;
 use Illuminate\Support\Facades\Route;
 
-
+$modulesPath = base_path('app/Modules');
+$modules = array_filter(glob($modulesPath . '/*'), 'is_dir');
+foreach ($modules as $modulePath) {
+    $moduleName = basename($modulePath);
+    $routesFile = $modulePath . '/Routes/web.php';
+    if (file_exists($routesFile)) {
+        require $routesFile;
+    }
+}
 
 Route::get('/tags', [TagController::class, 'index'])->name('tags.index');
 Route::resource('tags', TagController::class)->except('show', 'edit', 'update');
