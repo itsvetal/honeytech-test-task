7ed622a692ee7798bc1a70c5324016e7faacdad2
iTsVetal <vitaliikryskiv.development@gmail.com>
2025-11-15 19:20:46 +0200

    Added styles to the tag create page.

--- resources/views/tags/create.blade.php
+++ resources/views/tags/create.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Add tag</h2>
-    <form method="POST" action="{{ route('tags.store') }}">
-        @csrf
-        <input type="text" name="name" placeholder="Tag title" required>
-        <button type="submit">Create</button>
-    </form>
+    <div class="create-tag-container">
+        <h2>Add tag</h2>
+        <form method="POST" action="{{ route('tags.store') }}">
+            @csrf
+            <input type="text" name="name" placeholder="Tag title" required>
+            <button type="submit">Create</button>
+        </form>
+    </div>
 @endsection
