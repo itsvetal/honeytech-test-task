a09ce4e7a8da209a5fe4b4bb6fa2e3bcdef217f0
iTsVetal <vitaliikryskiv.development@gmail.com>
2025-11-15 18:38:11 +0200

    Added styles to the post show page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <img src="{{$post->thumbnail_url}}" alt="Post preview">
-    <h2>{{ $post->title }}</h2>
-    <p>{{ $post->content }}</p>
-    <p>Теги: @foreach($post->tags as $tag) <span class="tag">{{ $tag->name }}</span> @endforeach</p>
-    <a href="{{ route('posts.index') }}">Back</a>
+    <div class="post-show-container">
+        <div class="post-show-image-container">
+            <img src="{{$post->thumbnail_url}}" alt="Post preview">
+        </div>
+        <h2>{{ $post->title }}</h2>
+        <p>{{ $post->content }}</p>
+        <p>Теги: @foreach($post->tags as $tag)
+                <span class="tag">{{ $tag->name }}</span>
+            @endforeach</p>
+        <a href="{{ route('posts.index') }}">Back</a>
+    </div>
 @endsection
